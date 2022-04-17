e3f0373d490ee7b6576fc203b4fbf2e9a4b216f5
0xtrevor <zetazeros@protonmail.com>
2022-04-17 12:47:54 -0700

    SPDX-License-Identifier: BSL-1.1

---
+++ contracts/Vault.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "../libraries/CommonLibrary.sol";
+import "../libraries/ExceptionsLibrary.sol";
+import "../interfaces/vaults/IVault.sol";
+import "./VaultGovernance.sol";
