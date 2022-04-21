a397184451f98abed2386f973ceb0cd1656ebe69
fdx00 <trevorcolohan@gmail.com>
2022-04-21 01:26:47 -0700

    added initial validators

---
+++ test/Vault.test.ts
@@ -0,0 +1,22 @@
+// import { expect } from "chai";
+// import { ethers, deployments } from "hardhat";
+// import { BigNumber, Signer, utils } from "ethers";
+// import {
+//     ERC20,
+//     ERC20Vault,
+//     ProtocolGovernance,
+//     VaultRegistry,
+//     VaultFactory,
+//     ERC20VaultGovernance,
+//     AaveVault,
+//     GatewayVault,
+//     GatewayVaultGovernance,
+//     VaultGovernance,
+// } from "./library/Types";
+// import {
+//     deployERC20Tokens,
+//     deploySubVaultsXGatewayVaultSystem,
+//     deploySubVaultSystem,
+// } from "./library/Deployments";
+// import Exceptions from "./library/Exceptions";
+// import { sleep } from "./library/Helpers";
