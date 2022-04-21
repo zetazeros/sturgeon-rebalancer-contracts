7444ab58cf6a589dd32a7fcf698d80c6e4285419
fdx00 <trevorcolohan@gmail.com>
2022-04-21 01:31:01 -0700

    added initial validators

--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -20,3 +20,29 @@
 // } from "./library/Deployments";
 // import Exceptions from "./library/Exceptions";
 // import { sleep } from "./library/Helpers";
+
+// xdescribe("Vault", () => {
+//     let deployer: Signer;
+//     let user: Signer;
+//     let stranger: Signer;
+//     let treasury: Signer;
+//     let protocolGovernanceAdmin: Signer;
+//     let strategy: Signer;
+
+//     let token: ERC20;
+//     let ERC20Vault: ERC20Vault;
+//     let AnotherERC20Vault: ERC20Vault;
+//     let anotherERC20Token: ERC20;
+//     let AaveVault: ERC20Vault;
+//     let nftERC20: number;
+//     let nftAave: number;
+//     let gatewayVault: GatewayVault;
+//     let vaultRegistry: VaultRegistry;
+//     let gatewayNft: number;
+//     let gatewayVaultGovernance: GatewayVaultGovernance;
+//     let protocolGovernance: ProtocolGovernance;
+//     let differentERC20Vault: ERC20Vault;
+//     let ERC20VaultGovernance: VaultGovernance;
+//     let testEncodingContract: any;
+//     let testVault: any;
+//     let deployment: Function;
