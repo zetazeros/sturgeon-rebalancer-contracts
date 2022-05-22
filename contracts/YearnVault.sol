97057614a3828084b2fce1f10061d369a6aadf64
kd0x <zetazeros@protonmail.com>
2022-05-22 02:45:58 +0100

    inheritdoc IVault

--- contracts/YearnVault.sol
+++ contracts/YearnVault.sol
@@ -7,7 +7,7 @@ import "../interfaces/vaults/IYearnVault.sol";
 import "./IntegrationVault.sol";
 
 contract YearnVault is IYearnVault, IntegrationVault {
-    
+
     using SafeERC20 for IERC20;
     uint256 public constant DEFAULT_MAX_LOSS = 10000; // 10000%%
 
@@ -16,4 +16,14 @@ contract YearnVault is IYearnVault, IntegrationVault {
     /// @notice Yearn protocol vaults used by this contract
     function yTokens() external view returns (address[] memory) {
         return _yTokens;
+    }
+
+    function tvl() public view override returns (uint256[] memory minTokenAmounts, uint256[] memory maxTokenAmounts) {
+        address[] memory tokens = _vaultTokens;
+        minTokenAmounts = new uint256[](tokens.length);
+        for (uint256 i = 0; i < _yTokens.length; ++i) {
+            IYearnProtocolVault yToken = IYearnProtocolVault(_yTokens[i]);
+            minTokenAmounts[i] = (yToken.balanceOf(address(this)) * yToken.pricePerShare()) / (10**yToken.decimals());
+        }
+        maxTokenAmounts = minTokenAmounts;
     }
