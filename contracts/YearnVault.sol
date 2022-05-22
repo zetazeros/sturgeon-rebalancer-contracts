5f85023840270150452eaa27c1d02d194f65a23f
kd0x <zetazeros@protonmail.com>
2022-05-22 02:44:54 +0100

    added state variables

--- contracts/YearnVault.sol
+++ contracts/YearnVault.sol
@@ -8,4 +8,12 @@ import "./IntegrationVault.sol";
 
 contract YearnVault is IYearnVault, IntegrationVault {
     
+    using SafeERC20 for IERC20;
+    uint256 public constant DEFAULT_MAX_LOSS = 10000; // 10000%%
+
+    address[] private _yTokens;
+
+    /// @notice Yearn protocol vaults used by this contract
+    function yTokens() external view returns (address[] memory) {
+        return _yTokens;
     }
