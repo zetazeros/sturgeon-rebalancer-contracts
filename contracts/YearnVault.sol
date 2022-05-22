ef32e61c17f1f575857fc2864d1975409b80b2f2
kd0x <zetazeros@protonmail.com>
2022-05-22 02:42:41 +0100

    started yearn vault

---
+++ contracts/YearnVault.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.9;
+
+import "../interfaces/external/yearn/IYearnProtocolVault.sol";
+import "../interfaces/vaults/IYearnVaultGovernance.sol";
+import "../interfaces/vaults/IYearnVault.sol";
+import "./IntegrationVault.sol";
