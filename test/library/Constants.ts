09ccbcddaaad36db3ad3de28d11116c7fdc6aab1
fdx00 <trevorcolohan@gmail.com>
2022-06-19 11:15:37 +0100

    Fixes/chainsecurity and strategies

---
+++ test/library/Constants.ts
@@ -0,0 +1,7 @@
+export const UNIV3_VAULT_INTERFACE_ID: string = "0x88878d28";
+export const YEARN_VAULT_INTERFACE_ID: string = "0x073ab565";
+export const ERC20_ROOT_VAULT_INTERFACE_ID: string = "0x99be4880";
+export const ERC20_ROOT_VAULT_GOVERNANCE: string = "0x6a2c3330";
+export const ERC20_ROOT_VAULT_GOVERNANCE_INTERFACE_ID: string = "0x6a2c3330";
+export const UNIV3_VAULT_GOVERNANCE_INTERFACE_ID: string = "0x5d8c07fe";
+export const YEARN_VAULT_GOVERNANCE_INTERFACE_ID: string = "0x15482137";
